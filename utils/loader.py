8bc5d60fc1c3b86bfa0dda4f10596016aa589c4f
uisjuhahsudiidhewyv-maker <uisjuhahsudiidhewyv@gmail.com>
2026-09-06 22:35:46 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -2,12 +2,17 @@ from sources.toonbr import ToonBrSource
 from sources.mangaflix import MangaFlixSource
 from sources.mangalivreblog import MangaLivreBlogSource
 from sources.wolftoon import WolftoonSource
-from sources.mangadex import MangaDexSource
-from sources.mangaball import MangaBallSource
-from sources.taiyo import TaiyoSource
+from sources.codeflix_ninemanga import NineMangaSource
+from sources.codeflix_mangadex import MangaDexSource
 
-_sources={
- "ToonBr":ToonBrSource(), "MangaFlix":MangaFlixSource(), "MangaLivreBlog":MangaLivreBlogSource(), "Wolftoon":WolftoonSource(),
- "MangaBall (PT-BR)":MangaBallSource(), "MangaDex (PT-BR)":MangaDexSource(), "Taiyō (PT-BR)":TaiyoSource(),
+_sources = {
+    "ToonBr": ToonBrSource(),
+    "MangaFlix": MangaFlixSource(),
+    "MangaLivreBlog": MangaLivreBlogSource(),
+    "Wolftoon": WolftoonSource(),
+    "NineManga": NineMangaSource(),
+    "MangaDex (PT-BR)": MangaDexSource(),
 }
-def get_all_sources(): return _sources
+
+def get_all_sources():
+    return _sources
