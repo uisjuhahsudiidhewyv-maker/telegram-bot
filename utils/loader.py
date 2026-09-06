40827500309e9dd5d9ce6f808decd5a023aea3e6
uisjuhahsudiidhewyv-maker <uisjuhahsudiidhewyv@gmail.com>
2026-09-06 21:16:42 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -2,25 +2,12 @@ from sources.toonbr import ToonBrSource
 from sources.mangaflix import MangaFlixSource
 from sources.mangalivreblog import MangaLivreBlogSource
 from sources.wolftoon import WolftoonSource
-from sources.mangaball import MangaBallSource
 from sources.mangadex import MangaDexSource
-from sources.mangafire import MangaFireSource
-from sources.mangastop import MangaStopSource
-from sources.mangasbrasuka import MangasBrasukaSource
+from sources.mangaball import MangaBallSource
 from sources.taiyo import TaiyoSource
 
-_sources = {
-    "ToonBr": ToonBrSource(),
-    "MangaFlix": MangaFlixSource(),
-    "MangaLivreBlog": MangaLivreBlogSource(),
-    "Wolftoon": WolftoonSource(),
-    "MangaBall (PT-BR)": MangaBallSource(),
-    "MangaDex (PT-BR)": MangaDexSource(),
-    "MangaFire": MangaFireSource(),
-    "Mangastop": MangaStopSource(),
-    "MangasBrasuka": MangasBrasukaSource(),
-    "Taiyō (PT-BR)": TaiyoSource(),
+_sources={
+ "ToonBr":ToonBrSource(), "MangaFlix":MangaFlixSource(), "MangaLivreBlog":MangaLivreBlogSource(), "Wolftoon":WolftoonSource(),
+ "MangaBall (PT-BR)":MangaBallSource(), "MangaDex (PT-BR)":MangaDexSource(), "Taiyō (PT-BR)":TaiyoSource(),
 }
-
-def get_all_sources():
-    return _sources
+def get_all_sources(): return _sources
