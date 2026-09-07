ab75e4541bd31162b043fe9969eae8f2fb4f6688
uisjuhahsudiidhewyv-maker <uisjuhahsudiidhewyv@gmail.com>
2026-09-07 09:12:37 +0200

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -4,6 +4,7 @@ from sources.mangalivreblog import MangaLivreBlogSource
 from sources.wolftoon import WolftoonSource
 from sources.codeflix_mangadex import MangaDexCodeflixSource
 from sources.codeflix_ninemanga import NineMangaCodeflixSource
+from sources.nexustoons import NexusToonsSource
 
 _sources = {
     "ToonBr": ToonBrSource(),
@@ -12,6 +13,7 @@ _sources = {
     "Wolftoon": WolftoonSource(),
     "MangaDex (PT-BR)": MangaDexCodeflixSource(),
     "NineManga Brasil": NineMangaCodeflixSource(),
+    "NexusToons": NexusToonsSource(),
 }
 
 
